af6914c6c14b5244c12938698fdcedf7386a04c2
dami0806 <dami0804@naver.com>
2023-01-03 01:40:56 +0900

    사진 완료

--- MemberProfile/Models/MemberListManager.swift
+++ MemberProfile/Models/MemberListManager.swift
@@ -13,16 +13,17 @@ final class MemberListManager {
     //2 전체 멤버 리스트 만들기 (원래 배열에 멤버 생성해도 됨)
     func makeMembersListDatas() {
         membersList = [
-            Member(name: "홍길동", age: 20, phone: "010-1111-2222", address: "서울"),
-            Member(name: "임꺽정", age: 23, phone: "010-2222-3333", address: "서울"),
-            Member(name: "스티브", age: 50, phone: "010-1234-1234", address: "미국"),
-            Member(name: "쿡", age: 30, phone: "010-7777-7777", address: "캘리포니아"),
-            Member(name: "베조스", age: 50, phone: "010-2222-7777", address: "하와이"),
-            Member(name: "배트맨", age: 40, phone: "010-3333-1234", address: "고담씨티"),
-            Member(name: "조커", age: 40, phone: "010-4321-1234", address: "고담씨티"),
-            Member(name: "홍길동", age: 20, phone: "010-1111-2222", address: "서울"),
-            Member(name: "임꺽정", age: 23, phone: "010-2222-3333", address: "서울"),
-            Member(name: "스티브", age: 50, phone: "010-1234-1234", address: "미국")
+            Member(name: "닉", age: 20, phone: "010-1111-2222", address: "부산"),
+            Member(name: "라바", age: 23, phone: "010-2222-3333", address: "서울"),
+            Member(name: "매트", age: 50, phone: "010-1234-1234", address: "미국"),
+            Member(name: "밥", age: 30, phone: "010-7777-7777", address: "캘리포니아"),
+            Member(name: "보", age: 50, phone: "010-2222-7777", address: "하와이"),
+            Member(name: "설리반", age: 40, phone: "010-3333-1234", address: "경주"),
+            Member(name: "스폰지밥", age: 40, phone: "010-4321-1234", address: "수족관"),
+            Member(name: "제리", age: 20, phone: "010-1212-1112", address: "인천"),
+            Member(name: "토토로", age: 23, phone: "010-1233-3223", address: "시흥"),
+            Member(name: "포뇨", age: 50, phone: "010-3569-1234", address: "전주"),
+            Member(name: "핑구", age: 50, phone: "010-4321-1234", address: "남극")
         ]
     }
     
