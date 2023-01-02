7b544f899962a185bd96416165b7e6c9fcdd8e9d
dami0806 <dami0804@naver.com>
2023-01-02 21:44:59 +0900

    사진빼고 잘나옴

--- MemberProfile/Controllers/ViewController.swift
+++ MemberProfile/Controllers/ViewController.swift
@@ -14,6 +14,8 @@ final class ViewController: UIViewController {
     
     // MVC패턴을 위한 데이터 매니저 (배열 관리 - 데이터)
     var memberListManager = MemberListManager()
+    
+    
     //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,19 +24,32 @@ final class ViewController: UIViewController {
         setupTableView()
         setupNaviBar()
         setupDatas()
+        // 네비게이션바 설정관련
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()  // 불투명으로
+        appearance.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .systemBlue
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
         
+        // 네비게이션바 오른쪽 상단 버튼 설정
+       
         
     }
     
     func setupTableView(){
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.rowHeight = 60
         //셀 높이 설정
-        tableView.register(MyTableViewCell.self, forCellReuseIdentifier: "MemeberCell")
+        tableView.register(MyTableViewCell.self, forCellReuseIdentifier: "MemberCell")
     }
     
     //네비게이션 바
     func setupNaviBar() {
+        
+        
         
     }
     func setupDatas(){
@@ -58,7 +73,9 @@ final class ViewController: UIViewController {
 extension ViewController:UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //다음 화면으로 이동
-       // let datailVC = DetailViewController()
+    
+        // 다음 화면에 멤버를 전달
+        
         
         
     }
@@ -71,7 +88,14 @@ extension ViewController:UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       return UITableViewCell()
+        // (힙에 올라간)재사용 가능한 셀을 꺼내서 사용하는 메서드
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemberCell", for: indexPath) as! MyTableViewCell
+        
+        //셀에 멤버 전달(셀에서 didSet으로)
+        cell.member = memberListManager[indexPath.row]
+        cell.selectionStyle = .none
+        
+       return cell
         
     }
     
--- MemberProfile/Views/MyTableViewCell.swift
+++ MemberProfile/Views/MyTableViewCell.swift
@@ -5,48 +5,142 @@
 //  Created by 박다미 on 2023/01/01.
 //
 
+//코드로 cell 만듦.
 import UIKit
 
 class MyTableViewCell: UITableViewCell {
+    //MARK: - 멤버 저장속성 구현
+    // 멤버가 변할때마다 자동으로 업데이트 되도록 구현 didSet(속성 감시자)
+    
+    var member: Member? {
+        didSet {
+            //세팅 member 접근
+            guard var member = member else {return }
+            mainImageView.image = member.memberImage //optional binding 해서 ? 필요없음
+            memberNameLabel.text = member.name
+            addressLabel.text = member.address
+            
+            
+        }
+    }
 
+    //MARK: - UI구현
+    
     //이미지뷰 만들기
+    let mainImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
     //label 만들고 stackview 묶기
+    let memberNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let addressLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let stackView: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .vertical
+        sv.distribution = .fill
+        sv.alignment = .fill
+        sv.spacing = 5
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+        
+    }()
     
-  
-    
-    
-//생성자
-    
-    
-    //지정새성자: 저장속성 세팅하려고
-    
-    //지정생성자 사용하면 필수 생성자가 자동생성안되니 반드시 구현 해야함
+    //MARK: - 생성자 셋팅
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+    //지정새성자: 저장속성 세팅하려고
         super.init(style: .default, reuseIdentifier: reuseIdentifier)
-        //setupStackView()
+        setupStackView()
         
+        
+
     }
-    
+    //지정생성자 사용하면 필수 생성자가 자동생성안되니 반드시 구현 해야함
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+ 
+  
     
+    
+    func setupStackView() {
+        self.addSubview(mainImageView)
 
-    
-    
-    
-    //이건 스토리보드로 짤때 사용 -> 코드사용시 생성자 이용
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+        // 셀 위에 스택뷰 올리기
+        self.addSubview(stackView)
+        
+        // 스택뷰 위에 뷰들 올리기
+        stackView.addArrangedSubview(memberNameLabel)
+        stackView.addArrangedSubview(addressLabel)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    //MARK: - 오토레이아웃 셋팅
+    // 오토레이아웃 정하는 정확한 시점
+    override func updateConstraints() {
+        setConstraints()
+        super.updateConstraints()
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.mainImageView.clipsToBounds = true
+        self.mainImageView.layer.cornerRadius = self.mainImageView.frame.width / 2 //정확히 원으로 만들기
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            mainImageView.heightAnchor.constraint(equalToConstant: 40),
+            mainImageView.widthAnchor.constraint(equalToConstant: 40),
+            
+            // self.leadingAnchor로 잡는 것보다 self.contentView.leadingAnchor로 잡는게 더 정확함 ⭐️
+            // (cell은 기본뷰로 contentView를 가지고 있기 때문)
+            mainImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 10),
+            mainImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            memberNameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: mainImageView.trailingAnchor, constant: 20),
+            
+            // self.trailingAnchor로 잡는 것보다 self.contentView.trailingAnchor로 잡는게 더 정확함 ⭐️
+            // (cell은 기본뷰로 contentView를 가지고 있기 때문)
+            stackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: self.mainImageView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: self.mainImageView.bottomAnchor)
+        ])
+    }
+    
+    
+
+    
+    
+    
+//    //이건 스토리보드로 짤때 사용 -> 코드사용시 생성자 이용
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
 
 }
